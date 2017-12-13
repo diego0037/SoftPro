2fbc9af68fd869f7d5c6c167e2c10d4a1a6d800c
msalasch <msalasch97@gmail.com>
2017-12-13 14:00:38 -0600

    calcular ct y bugs corregidos

--- js/io.js
+++ js/io.js
@@ -7,6 +7,7 @@ function PedidoOptimo(d,s,h) {
     $('#qo').val(div);
     $('#qop').val(div);
     $('#qopg').val(div);
+    $('#qopct').val(div);
 }
 function PedidosAnuales(d,q) {
     div = Math.ceil(d/q);
@@ -20,28 +21,44 @@ function CostoGestionar(h,q,s,d){
     cg = h*(q/2)+s*(d/q)
     $('#costo_gestionar').text("Su costo de gestionar las unidades es de: " + cg.toFixed(2));
 }
+function CostoTotal(p,h,q,s,d){
+    debugger;
+    ct = p*q+h*(q/2)+s*(d/q)
+    $('#ct').text("Su costo total es de: " + ct.toFixed(2));
+}
+
 $(document).ready(function() {
 
     $('#calc').on('click', function(event) {
-        PedidoOptimo(parseInt($('#demand').val()), parseInt($('#cd').val()),parseInt($('#cm').val()));
+        PedidoOptimo(parseInt($('#demand').val()), parseInt($('#cd').val()),parseFloat($('#cm').val()));
         $('#costMan').val($('#cm').val());
         $('#demanda').val(parseInt($('#demand').val()));
 
         $('#demandg').val(parseInt($('#demand').val()));
         $('#cdg').val(parseInt($('#cd').val()));
         $('#cmg').val($('#cm').val());
+
+
+        $('#demandct').val(parseInt($('#demand').val()));
+        $('#cdct').val(parseInt($('#cd').val()));
+        $('#cmct').val($('#cm').val());
     });
 
     $('#pedidos').on('click', function(event) {
         PedidosAnuales(parseInt($('#demanda').val()), parseInt($('#qo').val()));
-    })
+    });
 
     $('#mantener').on('click', function(event) {
-        CostoMantener(parseInt($('#costMan').val()),parseInt($('#qop').val()));
-    })
+        CostoMantener(parseFloat($('#costMan').val()),parseInt($('#qop').val()));
+    });
 
     $('#gestionar').on('click', function(event) {
-        CostoGestionar(parseInt($('#cmg').val()),parseInt($('#qopg').val()),parseInt($('#cdg').val()),
+        CostoGestionar(parseFloat($('#cmg').val()),parseInt($('#qopg').val()),parseInt($('#cdg').val()),
         parseInt($('#demandg').val()));
-    })
+    });
+
+    $('#calc_ct').on('click', function(event) {
+        CostoTotal(parseFloat($('#pct').val()),parseFloat($('#cmct').val()),parseInt($('#qopct').val()),
+        parseInt($('#cdct').val()),parseInt($('#demandct').val()));
+    });
 })
